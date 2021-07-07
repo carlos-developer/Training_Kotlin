8a7d26dc1829007301f0046a90fff91366b16e81
juan carlos suarez marin <juan.suarez@gopenux.com>
2021-07-06 22:10:18 -0500

    #6 Add Picasso

--- app/src/main/java/com/example/training_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/training_kotlin/MainActivity.kt
@@ -19,17 +19,108 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         /** Lista añadida para probar el funcionamiento del adaptador*/
-        val list = listOf(User(1,"Juan", "Suarez", "url//:"),
-            User(2,"Pedro", "Suarez", "url//:"),
-            User(3,"Sara", "Suarez", "url//:"),
-            User(4,"Andrea", "Suarez", "url//:"),
-            User(5,"Giovana", "Suarez", "url//:"))
+
         /**------------------------------------------------------------*/
 
-        userAdapter = UserAdapter(list)
+        userAdapter = UserAdapter(getUsers())
         binding.recyclerViewUser.apply {
             layoutManager = linearLayoutManager
             adapter = userAdapter
         }
+    }
+
+    private fun getUsers(): List<User> {
+        return listOf(
+            User(
+                1,
+                "Juan",
+                "Suarez",
+                "https://live.staticflickr.com/7385/8993534463_241b0b6eb8_b.jpg"
+            ),
+            User(
+                2,
+                "Pedro",
+                "Suarez",
+                "https://upload.wikimedia.org/wikipedia/commons/c/c1/Bar%C3%A7a_-_Napoli_-_20140806_-_Pedro_Rodriguez.jpg"
+            ),
+            User(
+                3,
+                "Sara",
+                "Suarez",
+                "https://live.staticflickr.com/4101/4819040531_47dbbeff73_b.jpg"
+            ),
+            User(
+                4,
+                "Andrea",
+                "Suarez",
+                "https://upload.wikimedia.org/wikipedia/commons/0/07/Andrea_Sola.jpg"
+            ),
+            User(
+                5,
+                "Giovana",
+                "Suarez",
+                "https://assets-jpcust.jwpsrv.com/thumbnails/tix75cxd-720.jpg"
+            ),
+            User(
+                6,
+                "Juan",
+                "Suarez",
+                "https://live.staticflickr.com/7385/8993534463_241b0b6eb8_b.jpg"
+            ),
+            User(
+                7,
+                "Pedro",
+                "Suarez",
+                "https://upload.wikimedia.org/wikipedia/commons/c/c1/Bar%C3%A7a_-_Napoli_-_20140806_-_Pedro_Rodriguez.jpg"
+            ),
+            User(
+                8,
+                "Sara",
+                "Suarez",
+                "https://live.staticflickr.com/4101/4819040531_47dbbeff73_b.jpg"
+            ),
+            User(
+                9,
+                "Andrea",
+                "Suarez",
+                "https://upload.wikimedia.org/wikipedia/commons/0/07/Andrea_Sola.jpg"
+            ),
+            User(
+                10,
+                "Giovana",
+                "Suarez",
+                "https://assets-jpcust.jwpsrv.com/thumbnails/tix75cxd-720.jpg"
+            ),
+            User(
+                11,
+                "Juan",
+                "Suarez",
+                "https://live.staticflickr.com/7385/8993534463_241b0b6eb8_b.jpg"
+            ),
+            User(
+                12,
+                "Pedro",
+                "Suarez",
+                "https://upload.wikimedia.org/wikipedia/commons/c/c1/Bar%C3%A7a_-_Napoli_-_20140806_-_Pedro_Rodriguez.jpg"
+            ),
+            User(
+                13,
+                "Sara",
+                "Suarez",
+                "https://live.staticflickr.com/4101/4819040531_47dbbeff73_b.jpg"
+            ),
+            User(
+                14,
+                "Andrea",
+                "Suarez",
+                "https://upload.wikimedia.org/wikipedia/commons/0/07/Andrea_Sola.jpg"
+            ),
+            User(
+                15,
+                "Giovana",
+                "Suarez",
+                "https://assets-jpcust.jwpsrv.com/thumbnails/tix75cxd-720.jpg"
+            )
+        )
     }
 }
--- app/src/main/java/com/example/training_kotlin/UserAdapter.kt
+++ app/src/main/java/com/example/training_kotlin/UserAdapter.kt
@@ -1,32 +1,40 @@
 package com.example.training_kotlin
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.training_kotlin.databinding.LayoutItemUserBinding
+import com.squareup.picasso.Picasso
+
 
 class UserAdapter(private val users: List<User>) : RecyclerView.Adapter<UserAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
-        ViewHolder ( LayoutInflater.from(parent.context)
-                          .inflate(R.layout.layout_item_user,
-                              parent,
-                              false)
-        )
+    private lateinit var context: Context
+    private lateinit var layout: View
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+        layout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.layout_item_user, parent, false)
+        return ViewHolder(layout)
+    }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val user = users[position]
-
         with(holder) {
             binding.tvOrder.text = user.id.toString()
             binding.tvName.text = user.name
+            Picasso.get()
+                .load(user.url)
+                .into(binding.imgPhoto)
         }
     }
 
     override fun getItemCount(): Int = users.size
 
-    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val binding = LayoutItemUserBinding.bind(itemView)
     }
 }
